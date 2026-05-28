3db8ce4bf8320ebd12b24de0fe2798b85bf2d1ac
ahibbert <40676358+ahibbert@users.noreply.github.com>
2026-05-28 15:29:48 +1000

    Update functions to internal

--- R/diagnostics_topmodels.R
+++ R/diagnostics_topmodels.R
@@ -1,6 +1,15 @@
 # Silence NSE checks for ggplot mappings.
 utils::globalVariables(c("theoretical", "observed", "detrended", "midpoint", "root_diff", "band_lower", "band_upper", "time", "split_group"))
 
+#' Diagnostic generics for fitted longitudinal GAMLSS-copula models
+#'
+#' These generics dispatch to methods for `gamlss.longitudinal` objects and
+#' return either diagnostic plot objects, scoring summaries, or forecast data.
+#'
+#' @name topmodels_diagnostics
+#' @aliases pithist qqrplot wormplot rootogram proscore procast
+NULL
+
 #' @export
 pithist <- function(object, ...) {
   UseMethod("pithist")
