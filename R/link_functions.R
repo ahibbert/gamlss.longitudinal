48a58b4dd4237f162865ab40aff1e51924dadfd4
ahibbert <40676358+ahibbert@users.noreply.github.com>
2026-06-04 13:16:46 +1000

    Remove link functions form exported functions to user so that index file is a bit cleaner for the package.

--- R/link_functions.R
+++ R/link_functions.R
@@ -6,18 +6,12 @@
 #' used by `gamlss.longitudinal` parameter transformations.
 #'
 #' @name link_functions
-#' @aliases logit logit_inv dlogit logit28 logit28_inv dlogit28
-#'   log_2plus log_2plus_inv dlog_2plus dlog dlog_inv dlogit_inv
-#'   fisher_z fisher_z_inv dfisher_z_inv dlog_2plus_inv log_1plus
-#'   log_1plus_inv dlog_1plus dlog_1plus_inv gumbel_linkfun
-#'   gumbel_linkinv dgumbel_linkinv
+#' @noRd
 NULL
 
-#' @export
 logit <- function(x) {
   return(log(x/(1-x)))
 }
-#' @export
 logit_inv <- function(x) {
   return(
     if(all(is.nan(exp(x)/(1+exp(x))))) {
@@ -27,15 +21,12 @@ logit_inv <- function(x) {
     }
   )
 }
-#' @export
 dlogit <- function(x) {
   return(1/(x-(x^2)))
 }
-#' @export
 logit28 <- function(x) {
   return(log(x/(28-x)))
 }
-#' @export
 logit28_inv <- function(x) {
   return(
     if(is.nan(exp(x)/(1+exp(x)))) {
@@ -45,18 +36,15 @@ logit28_inv <- function(x) {
     }
   )
 }
-#' @export
 dlogit28 <- function(x) {
   return(1/(28*x-(x^2)))
 }
 
-#' @export
 log_2plus <- function(x) {
   return(
     log(x-2)
   )
 }
-#' @export
 log_2plus_inv <- function(x) {
   y=exp(x)+2
   #Adjust for error when close to two.
@@ -64,53 +52,43 @@ log_2plus_inv <- function(x) {
   y[close_to_boundary]=y[close_to_boundary]+0.00001
   return(y)
 }
-#' @export
 dlog_2plus <- function(x) {
   return(1/(x-2))
 }
-#' @export
 dlog <-function(x) {
   return(1/x)
 }
-#' @export
 dlog_inv <-function(x) {
   return(exp(x))
 }
-#' @export
 dlogit_inv <- function(x) {
   return(exp(x)/((1+exp(x))^2))
 }
 
-#' @export
 fisher_z <- function(x) {
   x <- pmin(pmax(x, -0.999999), 0.999999)
   return(atanh(x))
 }
 
-#' @export
 fisher_z_inv <- function(x) {
   return(tanh(x))
 }
 
-#' @export
 dfisher_z_inv <- function(x) {
   y <- tanh(x)
   return(1 - y^2)
 }
 
-#' @export
 dlog_2plus_inv <- function(x) {
   return(exp(x))
 }
 
-#' @export
 log_1plus <- function(x) {
   return(
     log(x-1)
   )
 }
 
-#' @export
 log_1plus_inv <- function(x) {
   y = exp(x) + 1
   close_to_boundary <- is.finite(y) & abs(y - 1) <= .Machine$double.eps
@@ -118,30 +96,25 @@ log_1plus_inv <- function(x) {
   return(y)
 }
 
-#' @export
 dlog_1plus <- function(x) {
   return(1/(x-1))
 }
 
-#' @export
 dlog_1plus_inv <- function(x) {
   return(exp(x))
 }
 
-#' @export
 gumbel_linkfun <- function(x) {
   x <- pmin(x, 17)
   return(log(x - 1))
 }
 
-#' @export
 gumbel_linkinv <- function(x) {
   y <- exp(x) + 1
   y[y > 17] <- 17
   return(y)
 }
 
-#' @export
 dgumbel_linkinv <- function(x) {
   y <- exp(x)
   y[y >= 16] <- 0
